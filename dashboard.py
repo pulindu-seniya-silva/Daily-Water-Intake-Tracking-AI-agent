c55cbd118c0b7c3c108468babcd03f29d39e3a03
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-07-31 19:57:36 +0530

    feat: add welcome section to AI water tracker dashboard

--- dashboard.py
+++ dashboard.py
@@ -6,4 +6,19 @@ from src.database import log_intake, get_intake_hostory
 
 if "tracker_started" not in st.session_state:
     st.session_state.tracker_started = False
+
+#Welcome section 
+
+if not st.session_state.tracker_started:
+    st.title("Welcome to AI water tracker")
+    st.markdown("""
+    Track your daily hydration with help of AI assistant.
+    log your intake, get smart feedback and stay healthy effortlessly
+    
+    """)
+    
+    if st.button(" start Tracking"):
+        st.session_state.tracker_started = True
+        st.experimental_rerun()
+
     
